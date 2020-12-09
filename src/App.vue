9ba404d8ae36f876418b74d1a8e4369eb0ccf449
epicmr <nhpeng1104@gmail.com>
2020-12-10 00:15:55 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Lesson1></Lesson1>
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Lesson1 from './components/Lesson1.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Lesson1
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Lesson1.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      Counter:{{counter}}
+  </div>
+</template>
+
+//基础模板
+<script>
+import { setInterval } from 'timers';
+export default {
+  name: 'Lesson1',
+  props: {
+    msg: String
+  },
+  data() {
+      return {
+          counter:0
+      }
+  },
+  mounted() {
+      setInterval(
+          () =>{
+              this.counter++
+        },1000)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
